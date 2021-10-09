adf9c14dc2ea73bec3140dd9666171a104707ff9
E9965-Kenny <63435466+e9965@users.noreply.github.com>
2021-10-09 19:13:49 +0800

    Update Container.js

--- components/Container.js
+++ components/Container.js
@@ -61,7 +61,7 @@ const Container = ({ children, layout, fullWidth, ...customMeta }) => {
             <meta property="article:author" content={BLOG.author} />
           </>
         )}
-      <script type="application/json" src="https://raw.githubusercontent.com/e9965/nobelium/main/times.js"></script>
+      <script src="https://rawgithubusercontent.com/e9965/nobelium/main/times.js"></script>
       </Head>
       <div
         className={`wrapper ${
