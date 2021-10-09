a588a6c8af6825946f8e226d005c3f035122ddb7
E9965-Kenny <63435466+e9965@users.noreply.github.com>
2021-10-09 20:40:04 +0800

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -14,10 +14,10 @@ const Footer = ({ fullWidth }) => {
       <div className="my-4 text-sm leading-6">
         <div className="flex align-baseline justify-between flex-wrap">
           <p>
-            © {BLOG.author} {from === y || !from ? y : `${from} - ${y}`}-<span id="times">
+            © {BLOG.author} {from === y || !from ? y : `${from} - ${y}`}-<span id="times"></span>
           </p>
           <script type="text/javascript" src="https://raw.githack.com/e9965/nobelium/main/times.js" ></script>
-          </span><div id="yy520">0</div>
+          <div id="yy520">0</div>
           <script type="text/javascript" src="https://raw.githack.com/e9965/nobelium/main/hitokoto.js" ></script>
           <Vercel />
         </div>
