1026abaa1a700f82ae4f10ad303407a07f80eb32
E9965-Kenny <63435466+e9965@users.noreply.github.com>
2021-10-09 20:50:09 +0800

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -14,7 +14,7 @@ const Footer = ({ fullWidth }) => {
       <div className="my-4 text-sm leading-6">
         <div className="flex align-baseline justify-between flex-wrap">
           <p>
-            © {BLOG.author} {from === y || !from ? y : `${from} - ${y}`}-<span id="times"></span>
+            © {BLOG.author} {from === y || !from ? y : `${from} - ${y}`}-<div id="times"></div>
           </p>
           <script type="text/javascript" src="https://raw.githack.com/e9965/nobelium/main/times.js" ></script>
           <p>
