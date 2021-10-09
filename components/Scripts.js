1af1309b4cf2864b5cd3d4849373306f3b959665
E9965-Kenny <63435466+e9965@users.noreply.github.com>
2021-10-09 18:49:37 +0800

    Update Scripts.js

--- components/Scripts.js
+++ components/Scripts.js
@@ -4,26 +4,7 @@ import BLOG from '@/blog.config'
 const Scripts = () => (
   <>
     <script src="https://blog.co2fe.cf/usr/waifu/auto_waifu.js"></script>
-  <script type="text/javascript">
-var s1 = "2022-04-23";//设置为你的建站时间
-s1 = new Date(s1.replace(/-/g, "/"));
-s2 = new Date();
-var days = s1.getTime() - s2.getTime();
-days =parseInt( days / (1000 * 60 * 60 * 24));
-var number_of_years = parseInt(days/365);
-days%=365;
-var number_of_months = parseInt(days/30);
-days%=30;
-var number_of_days = parseInt(days);
-var timeHTML="距離DSE還剩下：";
-if(number_of_years)
-    timeHTML+=number_of_years+"年";
-if(number_of_months)
-    timeHTML+=number_of_months+"月";
-if(number_of_days)
-    timeHTML+=number_of_days+"天。";
-document.getElementById('times').innerHTML = timeHTML;
-  </script>
+  <script src="https://raw.githubusercontent.com/e9965/nobelium/main/times.js"></script>
     {BLOG.analytics && BLOG.analytics.provider === 'ackee' && (
       <Script
         src={BLOG.analytics.ackeeConfig.tracker}
