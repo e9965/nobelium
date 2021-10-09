404205a43ece4f015f3e667485a4ac3bb6cc810d
E9965-Kenny <63435466+e9965@users.noreply.github.com>
2021-10-09 19:18:30 +0800

    Update Container.js

--- components/Container.js
+++ components/Container.js
@@ -61,7 +61,26 @@ const Container = ({ children, layout, fullWidth, ...customMeta }) => {
             <meta property="article:author" content={BLOG.author} />
           </>
         )}
-      <script type="text/javascript" src="https://rawgithubusercontent.com/e9965/nobelium/main/times.js"></script>
+      <script type="text/javascript">
+var s1 = "2022-04-23";
+s1 = new Date(s1.replace(/-/g, "/"));
+s2 = new Date();
+var days = s1.getTime() - s2.getTime();
+days =parseInt( days / (1000 * 60 * 60 * 24));
+var number_of_years = parseInt(days/365);
+days%=365;
+var number_of_months = parseInt(days/30);
+days%=30;
+var number_of_days = parseInt(days);
+var timeHTML="距離DSE還剩下：";
+if(number_of_years)
+    timeHTML+=number_of_years+"年";
+if(number_of_months)
+    timeHTML+=number_of_months+"月";
+if(number_of_days)
+    timeHTML+=number_of_days+"天。";
+document.getElementById('times').innerHTML = timeHTML;
+      </script>
       </Head>
       <div
         className={`wrapper ${
